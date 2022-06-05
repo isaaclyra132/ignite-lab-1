a07c224d7d6b4f2d72c9a9cda5459275008b1af2
Isaac de Lyra Jr <isaac.lyra.junior@gmail.com>
2022-06-05 00:48:03 -0300

    web page started

---
+++ web/src/pages/_app.tsx
@@ -0,0 +1,9 @@
+import { UserProvider } from '@auth0/nextjs-auth0';
+
+function MyApp({ Component, pageProps }) {
+  <UserProvider>
+    <Component {...pageProps} />
+  </UserProvider>
+}
+
+export default MyApp
